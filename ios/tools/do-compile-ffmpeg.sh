fc1236b9496ec1ecbdf6c8b7040abe53507747af
Bunny Lin <hearther.lin@gmail.com>
2016-12-22 11:24:38 +0800

    add support to x265 and fdk aac

--- ios/tools/do-compile-ffmpeg.sh
+++ ios/tools/do-compile-ffmpeg.sh
@@ -97,6 +97,8 @@ case "$FF_BUILD_OPT" in
     ;;
 esac
 
+
+
 echo "build_root: $FF_BUILD_ROOT"
 
 #--------------------
@@ -124,18 +126,24 @@ FF_XCODE_BITCODE=
 if [ "$FF_ARCH" = "i386" ]; then
     FF_BUILD_NAME="ffmpeg-i386"
     FF_BUILD_NAME_OPENSSL=openssl-i386
+    FF_BUILD_NAME_X264=x264-i386
+    FF_BUILD_NAME_FDK_AAC=AAC-i386
     FF_XCRUN_PLATFORM="iPhoneSimulator"
     FF_XCRUN_OSVERSION="-mios-simulator-version-min=6.0"
     FFMPEG_CFG_FLAGS="$FFMPEG_CFG_FLAGS $FFMPEG_CFG_FLAGS_SIMULATOR"
 elif [ "$FF_ARCH" = "x86_64" ]; then
     FF_BUILD_NAME="ffmpeg-x86_64"
     FF_BUILD_NAME_OPENSSL=openssl-x86_64
+    FF_BUILD_NAME_X264=x264-x86_64
+    FF_BUILD_NAME_FDK_AAC=AAC-x86_64
     FF_XCRUN_PLATFORM="iPhoneSimulator"
     FF_XCRUN_OSVERSION="-mios-simulator-version-min=7.0"
     FFMPEG_CFG_FLAGS="$FFMPEG_CFG_FLAGS $FFMPEG_CFG_FLAGS_SIMULATOR"
 elif [ "$FF_ARCH" = "armv7" ]; then
     FF_BUILD_NAME="ffmpeg-armv7"
     FF_BUILD_NAME_OPENSSL=openssl-armv7
+    FF_BUILD_NAME_X264=x264-armv7
+    FF_BUILD_NAME_FDK_AAC=AAC-armv7
     FF_XCRUN_OSVERSION="-miphoneos-version-min=6.0"
     FF_XCODE_BITCODE="-fembed-bitcode"
     FFMPEG_CFG_FLAGS="$FFMPEG_CFG_FLAGS $FFMPEG_CFG_FLAGS_ARM"
@@ -143,6 +151,8 @@ elif [ "$FF_ARCH" = "armv7" ]; then
 elif [ "$FF_ARCH" = "armv7s" ]; then
     FF_BUILD_NAME="ffmpeg-armv7s"
     FF_BUILD_NAME_OPENSSL=openssl-armv7s
+    FF_BUILD_NAME_X264=x264-armv7s
+    FF_BUILD_NAME_FDK_AAC=AAC-armv7s
     FFMPEG_CFG_CPU="--cpu=swift"
     FF_XCRUN_OSVERSION="-miphoneos-version-min=6.0"
     FF_XCODE_BITCODE="-fembed-bitcode"
@@ -150,6 +160,8 @@ elif [ "$FF_ARCH" = "armv7s" ]; then
 elif [ "$FF_ARCH" = "arm64" ]; then
     FF_BUILD_NAME="ffmpeg-arm64"
     FF_BUILD_NAME_OPENSSL=openssl-arm64
+    FF_BUILD_NAME_X264=x264-arm64
+    FF_BUILD_NAME_FDK_AAC=AAC-arm64
     FF_XCRUN_OSVERSION="-miphoneos-version-min=7.0"
     FF_XCODE_BITCODE="-fembed-bitcode"
     FFMPEG_CFG_FLAGS="$FFMPEG_CFG_FLAGS $FFMPEG_CFG_FLAGS_ARM"
@@ -209,6 +221,37 @@ if [ -f "${FFMPEG_DEP_OPENSSL_LIB}/libssl.a" ]; then
     FFMPEG_CFLAGS="$FFMPEG_CFLAGS -I${FFMPEG_DEP_OPENSSL_INC}"
     FFMPEG_DEP_LIBS="$FFMPEG_CFLAGS -L${FFMPEG_DEP_OPENSSL_LIB} -lssl -lcrypto"
 fi
+
+#--------------------
+echo "\n--------------------"
+echo "[*] check x264"
+echo "----------------------"
+FFMPEG_DEP_X264_INC=$FF_BUILD_ROOT/build/$FF_BUILD_NAME_X264/output/include
+FFMPEG_DEP_X264_LIB=$FF_BUILD_ROOT/build/$FF_BUILD_NAME_X264/output/lib
+#--------------------
+# with x264
+if [ -f "${FFMPEG_DEP_X264_LIB}/libx264.a" ]; then
+    FFMPEG_CFG_FLAGS="$FFMPEG_CFG_FLAGS --enable-gpl --enable-libx264 --enable-encoder=libx264"
+
+    FFMPEG_CFLAGS="$FFMPEG_CFLAGS -I${FFMPEG_DEP_X264_INC}"
+    FFMPEG_DEP_LIBS="$FFMPEG_CFLAGS -L${FFMPEG_DEP_X264_LIB} -lx264"
+fi
+
+#--------------------
+echo "\n--------------------"
+echo "[*] check fdk aac"
+echo "----------------------"
+FFMPEG_DEP_AAC_INC=$FF_BUILD_ROOT/build/$FF_BUILD_NAME_AAC/output/include
+FFMPEG_DEP_AAC_LIB=$FF_BUILD_ROOT/build/$FF_BUILD_NAME_AAC/output/lib
+#--------------------
+# with fdk aac
+if [ -f "${FFMPEG_DEP_AAC_LIB}/libfdk-aac.a" ]; then
+    FFMPEG_CFG_FLAGS="$FFMPEG_CFG_FLAGS --enable-libfdk-aac --enable-nonfree"
+
+    FFMPEG_CFLAGS="$FFMPEG_CFLAGS -I${FFMPEG_DEP_AAC_INC}"
+    FFMPEG_DEP_LIBS="$FFMPEG_CFLAGS -L${FFMPEG_DEP_AAC_LIB} -lfdk-aac"
+fi
+
 
 #--------------------
 echo "\n--------------------"
