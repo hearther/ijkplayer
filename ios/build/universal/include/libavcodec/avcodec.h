05b4d9d1db706aaca6c44b83486315ae27c966ee
hearther <hearther.lin@gmail.com>
2017-06-13 17:34:31 +0800

    update for ios 11

--- ios/build/universal/include/libavcodec/avcodec.h
+++ ios/build/universal/include/libavcodec/avcodec.h
@@ -565,7 +565,7 @@ enum AVCodecID {
  */
 typedef struct AVCodecDescriptor {
     enum AVCodecID     id;
-    enum AVMediaType type;
+    enum FFMpegAVMediaType type;
     /**
      * Name of the codec described by this descriptor. It is non-empty and
      * unique for each codec descriptor. It should contain alphanumeric
@@ -1508,7 +1508,7 @@ typedef struct AVCodecContext {
     const AVClass *av_class;
     int log_level_offset;
 
-    enum AVMediaType codec_type; /* see AVMEDIA_TYPE_xxx */
+    enum FFMpegAVMediaType codec_type; /* see AVMEDIA_TYPE_xxx */
     const struct AVCodec  *codec;
 #if FF_API_CODEC_NAME
     /**
@@ -3483,7 +3483,7 @@ typedef struct AVCodec {
      * You should use the NULL_IF_CONFIG_SMALL() macro to define it.
      */
     const char *long_name;
-    enum AVMediaType type;
+    enum FFMpegAVMediaType type;
     enum AVCodecID id;
     /**
      * Codec capabilities.
@@ -3588,7 +3588,7 @@ typedef struct AVHWAccel {
      *
      * See AVMEDIA_TYPE_xxx
      */
-    enum AVMediaType type;
+    enum FFMpegAVMediaType type;
 
     /**
      * Codec implemented by the hardware accelerator.
@@ -5563,7 +5563,7 @@ int av_lockmgr_register(int (*cb)(void **mutex, enum AVLockOp op));
 /**
  * Get the type of the given codec.
  */
-enum AVMediaType avcodec_get_type(enum AVCodecID codec_id);
+enum FFMpegAVMediaType avcodec_get_type(enum AVCodecID codec_id);
 
 /**
  * Get the name of a codec.
--- ios/build/universal/include/libavutil/i386/ffversion.h
+++ ios/build/universal/include/libavutil/i386/ffversion.h
@@ -1,4 +1,4 @@
 #ifndef AVUTIL_FFVERSION_H
 #define AVUTIL_FFVERSION_H
-#define FFMPEG_VERSION "ff2.8--ijk0.4.4.1--dev0.3.3--rc7"
+#define FFMPEG_VERSION "ff2.8--ijk0.4.4.1--dev0.3.3--rc7-1-g99fd4b8d8f"
 #endif /* AVUTIL_FFVERSION_H */
