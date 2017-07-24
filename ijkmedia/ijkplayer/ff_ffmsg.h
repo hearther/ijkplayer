2a0ad08edd045df5629cae01ec445c1b39b56221
raymondzheng <raymondzheng1412@gmail.com>
2017-07-24 18:24:47 +0800

    ijkplayer: support get frame at time

--- ijkmedia/ijkplayer/ff_ffmsg.h
+++ ijkmedia/ijkplayer/ff_ffmsg.h
@@ -43,7 +43,7 @@
 #define FFP_MSG_PLAYBACK_STATE_CHANGED      700
 #define FFP_MSG_TIMED_TEXT                  800
 #define FFP_MSG_ACCURATE_SEEK_COMPLETE      900     /* arg1 = current position*/
-#define FFP_MSG_ARTWORK                     1000     /* arg1 = artwork data len, arg2 = 0, obj = artwork data, arg4 = artwork data len */
+#define FFP_MSG_GET_IMG_STATE               1000    /* arg1 = timestamp, arg2 = result code, obj = file name*/
 
 #define FFP_MSG_VIDEO_DECODER_OPEN          10001
 
