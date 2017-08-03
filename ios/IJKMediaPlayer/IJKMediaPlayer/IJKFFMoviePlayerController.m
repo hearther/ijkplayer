3b7d200df71c6d63e01e7ec7cabf5ae943b8db5b
hearther <hearther.lin@gmail.com>
2017-08-03 15:30:13 +0800

    fix

--- ios/IJKMediaPlayer/IJKMediaPlayer/IJKFFMoviePlayerController.m
+++ ios/IJKMediaPlayer/IJKMediaPlayer/IJKFFMoviePlayerController.m
@@ -33,7 +33,7 @@
 #import "ijkioapplication.h"
 #include "string.h"
 
-static const char *kIJKFFRequiredFFmpegVersion = "ff3.2--ijk0.7.6--20170324--001";
+static const char *kIJKFFRequiredFFmpegVersion = "3f19c25412bfe91da9f41037999498ba91aee726";
 
 // It means you didn't call shutdown if you found this object leaked.
 @interface IJKWeakHolder : NSObject
